c63746cc57ee0d6aff747addb5be6e982d991437
surrealsupernovae <surrealsupernovae@disr.it>
2024-05-26 02:58:30 +0200

    funt

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,5 +1,10 @@
+fn five() -> i32 {
+	5
+}  
 fn main() {
     another_function(40, 'm');
+    let funf = five();
+    println!(" five is {funf}");
 }
 
 fn another_function (value: u32, unit_label: char) {
