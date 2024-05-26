867672d68c15f08066ca3acc865899dad715ff16
surrealsupernovae <surrealsupernovae@disr.it>
2024-05-26 02:46:01 +0200

    funct

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    another_function(40, 'm');
+}
+
+fn another_function (value: u32, unit_label: char) {
+	println!("The measurement is: {value} {unit_label}");
 }
