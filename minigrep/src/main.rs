1e05e43baa3e1333a33c3a4a81389432e5ee3334
surrealsupernovae <surrealsupernovae@disr.it>
2024-07-11 16:23:32 +0200

    mini_grep changes

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::env 
+
+fn main {
+	let args: Vec<String> = env::args().collect();
+	let query = &args[1];
+	let path = &args[2];
+	
 }
---
+++ minigrep/target/debug/deps/minigrep-b01c23144c311f04.d
@@ -0,0 +1,9 @@
+/home/surrealsupernovae/Rust/learning_rust/minigrep/target/debug/deps/libminigrep-b01c23144c311f04.rmeta: src/main.rs Cargo.toml
+
+/home/surrealsupernovae/Rust/learning_rust/minigrep/target/debug/deps/minigrep-b01c23144c311f04.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
