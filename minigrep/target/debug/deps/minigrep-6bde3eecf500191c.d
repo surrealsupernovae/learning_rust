7259c2843361c823cdbd98692659e5e7fe4460b9
raphynovae <116360380+raphynovae@users.noreply.github.com>
2024-08-12 20:07:47 +0200

    minigrep changes

--- minigrep/target/debug/deps/minigrep-6bde3eecf500191c.d
+++ minigrep/target/debug/deps/minigrep-6bde3eecf500191c.d
@@ -1,6 +1,6 @@
-/Users/raphy/offline/minigrep/target/debug/deps/libminigrep-6bde3eecf500191c.rmeta: src/main.rs Cargo.toml
+/Users/raphy/Projects/minigrep/target/debug/deps/libminigrep-6bde3eecf500191c.rmeta: src/main.rs Cargo.toml
 
-/Users/raphy/offline/minigrep/target/debug/deps/minigrep-6bde3eecf500191c.d: src/main.rs Cargo.toml
+/Users/raphy/Projects/minigrep/target/debug/deps/minigrep-6bde3eecf500191c.d: src/main.rs Cargo.toml
 
 src/main.rs:
 Cargo.toml:
--- minigrep/target/debug/deps/minigrep-f993ae18fe818960.d
+++ minigrep/target/debug/deps/minigrep-f993ae18fe818960.d
@@ -1,6 +1,6 @@
-/Users/raphy/offline/minigrep/target/debug/deps/libminigrep-f993ae18fe818960.rmeta: src/lib.rs Cargo.toml
+/Users/raphy/Projects/minigrep/target/debug/deps/libminigrep-f993ae18fe818960.rmeta: src/lib.rs Cargo.toml
 
-/Users/raphy/offline/minigrep/target/debug/deps/minigrep-f993ae18fe818960.d: src/lib.rs Cargo.toml
+/Users/raphy/Projects/minigrep/target/debug/deps/minigrep-f993ae18fe818960.d: src/lib.rs Cargo.toml
 
 src/lib.rs:
 Cargo.toml:
