207bb7b4ffb7587eedb4b2175689017314262541
surrealsupernovae <surrealsupernovae@disr.it>
2024-10-31 18:28:39 +0100

    update

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,8 +1,16 @@
-use std::env 
+use std::env;
 
-fn main {
+fn main () {
 	let args: Vec<String> = env::args().collect();
+
+	let (query, path) = parse_config (&args);
+	  
+}
+
+fn parse_config ( args: &[String]) -> (&str, &str) {
 	let query = &args[1];
 	let path = &args[2];
+
+	(query,path)
 	
 }
